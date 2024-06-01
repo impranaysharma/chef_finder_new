78e59e44c7d3c02b4a8b6597d90a1cd747923250
Pranay Sharma <121673814+impranaysharma@users.noreply.github.com>
2024-06-01 14:25:04 +0530

    Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,160 +1,37 @@
-import React, { useState } from 'react';
-import { MDBCollapse, MDBBtn,MDBValidation,
-    MDBValidationItem,
-    MDBInput,
-    MDBInputGroup,
+import React from 'react'
+import './Form.css'
+const Form = () => {
+  return (<><div id='wh'>
+    <div id='fhead'> Join Us</div>
+    <form id='fwork'
+    method="POST" 
+    action="https://script.google.com/macros/s/AKfycbwgwRUxxL-pG8Knas8UGSi-5tr5hoORkChd-WF-ot8q2lsKNM2bIZwr2PRr0cXwizJqaw/exec"
+  >
+    <label id='lemail'>Email : </label>
+    <input name="Email" type="email" placeholder="Email" required id='email'/><br/>
+    <label id='lname'>Name : </label>
+    
+    <input name="Name" type="text" placeholder="Name" required id='name'/><br/>
+    <label id='lnumber'> Number : </label>
+    <input name='Integer' type='integer' placeholder='Number' required id='integer'/><br/>
+    <div>
+      <input type="radio" id="contactChoice1" name="contact" value="chef" />
+      <label for="contactChoice1">Chef</label>
 
-    MDBCheckbox } from 'mdb-react-ui-kit';
-import './Section.css'
-export default function Section() {
-  const [isOpen, setIsOpen] = useState(false);
+      <input type="radio" id="contactChoice2" name="contact" value="employee" />
+      <label for="contactChoice2">Employee</label>
 
-  const toggleOpen = () => setIsOpen(!isOpen);
-  const [isOpen1, setIsOpen1] = useState(false);
-
-  const toggleOpen1 = () => setIsOpen1(!isOpen);
-  const [formValue, setFormValue] = useState({
-    fname: 'Devanshu',
-    lname: '',
-    email: 'tba',
-    city: 'Greater Noida',
-    state: 'Uttar Pradesh',
-    zip: '301301',
-  });
-  const onChange = (e: any) => {
-    setFormValue({ ...formValue, [e.target.name]: e.target.value });
-  };
-
-  return (
-    < >
-    <div className='section'>
-      <h1 id='shead'> Define Your Self</h1>
-      <MDBBtn onClick={toggleOpen} id='chef'>Chef</MDBBtn>
-      <MDBBtn onClick={toggleOpen1} id='employee'>Employee</MDBBtn>
-      <MDBCollapse open={isOpen}>
-      <MDBValidation className='row g-3'>
-      <MDBValidationItem className='col-md-4'>
-        <MDBInput
-          value={formValue.fname}
-          name='fname'
-          onChange={onChange}
-          id='validationCustom01'
-          required
-          label='First name'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-4'>
-        <MDBInput
-          value={formValue.lname}
-          name='lname'
-          onChange={onChange}
-          id='validationCustom02'
-          required
-          label='Last name'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem feedback='Please choose a username.' invalid className='col-md-4'>
-        <MDBInputGroup textBefore='@'>
-          <input
-            type='text'
-            className='form-control'
-            id='validationCustomUsername'
-            placeholder='Username'
-            required
-          />
-        </MDBInputGroup>
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-6' feedback='Please provide a valid city.' invalid>
-        <MDBInput
-          value={formValue.city}
-          name='city'
-          onChange={onChange}
-          id='validationCustom03'
-          required
-          label='City'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-6' feedback='Please provide a valid zip.' invalid>
-        <MDBInput
-          value={formValue.zip}
-          name='zip'
-          onChange={onChange}
-          id='validationCustom05'
-          required
-          label='Zip'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-12' feedback='You must agree before submitting.' invalid>
-        <MDBCheckbox label='Agree to terms and conditions' id='invalidCheck' required />
-      </MDBValidationItem>
-      <div className='col-12'>
-        <MDBBtn type='submit'>Submit form</MDBBtn>
-        <MDBBtn type='reset'>Reset form</MDBBtn>
-      </div>
-    </MDBValidation></MDBCollapse>
-      
-      <MDBCollapse open={isOpen1}>
-      <MDBValidation className='row g-3'>
-      <MDBValidationItem className='col-md-4'>
-        <MDBInput
-          value={formValue.fname}
-          name='Animesh'
-          onChange={onChange}
-          id='validationCustom01'
-          required
-          label='First name'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-4'>
-        <MDBInput
-          value={formValue.lname}
-          name='lname'
-          onChange={onChange}
-          id='validationCustom02'
-          required
-          label='Last name'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem feedback='Please choose a username.' invalid className='col-md-4'>
-        <MDBInputGroup textBefore='@'>
-          <input
-            type='text'
-            className='form-control'
-            id='validationCustomUsername'
-            placeholder='Username'
-            required
-          />
-        </MDBInputGroup>
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-6' feedback='Please provide a valid city.' invalid>
-        <MDBInput
-          value={formValue.city}
-          name='city'
-          onChange={onChange}
-          id='validationCustom03'
-          required
-          label='City'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-md-6' feedback='Please provide a valid zip.' invalid>
-        <MDBInput
-          value={formValue.zip}
-          name='zip'
-          onChange={onChange}
-          id='validationCustom05'
-          required
-          label='Zip'
-        />
-      </MDBValidationItem>
-      <MDBValidationItem className='col-12' feedback='You must agree before submitting.' invalid>
-        <MDBCheckbox label='Agree to terms and conditions' id='invalidCheck' required />
-      </MDBValidationItem>
-      <div className='col-12'>
-        <MDBBtn type='submit'>Submit form</MDBBtn>
-        <MDBBtn type='reset'>Reset form</MDBBtn>
-      </div>
-    </MDBValidation></MDBCollapse></div>
-      
-    </>
-  );
+    </div>
+    <label id='lnumber4'> Number of Referral: </label>
+    <input name="Referral" type="text" placeholder="Refferal Phone Number" id='name'/><br/>
+    
+    <div>
+    </div>
+    <button  type="submit" id='send'>Submit</button>
+  </form>
+  </div>
+  </>
+  )
 }
+
+export default Form
